1f6b84af541eb56032399a989ebeaff3dc9f6459
Delegat-is <kelvinjuma903@gmail.com>
2023-03-27 14:45:11 -0700

    function that prints an integer with {:d}.format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    while True:
         try:
-            x = int(input("Please enter an integer value: "))
-            print("{:d}".format(x))
-            break
-        except ValueError:
-            print("False")
+            print("{:d}".format(value))
+            return True
+        except (ValueError, TypeError):
+            return False
