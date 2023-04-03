3485b86f2158b1ef3c1e08264714d18f6f8f1d20
Delegat-is <kelvinjuma903@gmail.com>
2023-04-03 14:01:05 -0700

    string representation of the rectangle to be able to recreate a new instance

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+""" class Rectangle defining a rectangle """
+
+
+class Rectangle:
+    """ Representation of an empty class Rectangle """
+
+    def __init__(self, width=0, height=0):
+        """ Initialization of a new rectangle.
+        Args:
+        @width(int): The rectangle's width
+        @height(int): The rectangle's height
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Retrieve the rectangle's width/ setting the rectangle's width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ A private instance width attribute """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getting the rectangle's height/ setting the rectangle's height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ A private instance height attribute """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Area of the rectangle"""
+        return (self.width * self.height)
+
+    def perimeter(self):
+        """ Perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            p = 0
+        else:
+            p = ((2 * self.width) + (2 * self.height))
+        return p
+
+    def __str__(self):
+        """ Returning a string pintable presentation of the rectangle.
+        Using the #character to represent the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return None
+
+        """Initialize an empty string representation of the rectangle"""
+        rect = ""
+        for i in range(self.height):
+            row_str = "#" * self.__width
+            rect += row_str
+            if i != self.__height -1:
+                rect += "\n"
+        return rect
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}{self.__height})"
