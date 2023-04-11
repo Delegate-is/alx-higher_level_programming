03250b42341e65a3ff6b74a539361d0ec012fd88
Delegat-is <kelvinjuma903@gmail.com>
2023-04-11 06:05:06 -0700

    isinstance now really not type

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,4 +6,4 @@ is_kind_of_class function representation
 def is_kind_of_class(obj, a_class):
     """Returns true if object is an instance of inherited class
     Otherwise, false"""
-    return isinstance(obj, a_class)
+    return (isinstance(obj, a_class))
