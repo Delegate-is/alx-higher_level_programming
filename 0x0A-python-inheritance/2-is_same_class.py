8fd308065fae377115b96812f495a0fa308f8aa2
Delegat-is <kelvinjuma903@gmail.com>
2023-04-11 05:58:05 -0700

    isinstance saved the day != really

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,8 @@
 is_same_class function representation
 """
 
+
 def is_same_class(obj, a_class):
     """Returns true give that object is exactly an instance in specified
     class. Otherwise, False."""
-    return type(obj) == a_class
+    return (type(obj) == a_class)
