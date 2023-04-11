4dac92ebc1d9056370ed49bc043afe93ad51ee8f
Delegat-is <kelvinjuma903@gmail.com>
2023-04-11 05:32:58 -0700

    lookup(list)

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Loookup function representation
+"""
+
+def lookup(obj):
+    """lookup(obj): Function that returns a method's and attribute's list
+    for an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,4 @@
+Inheritance and Multiple Inheritance.
+Python Language
+Project 0x0A. Python - Inheritance
+On 11/04/2023
