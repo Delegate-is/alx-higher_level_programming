99771d525432e65e3caceae695c9adedc03fb380
Delegat-is <kelvinjuma903@gmail.com>
2023-04-11 06:24:35 -0700

    issubclass with type

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ inherits_from function representation
 def inherits_from(obj, a_class):
     """ Returns true if object is an instance of inherited class.
     Otherwise, false"""
-    return (issubclass(type(obj), a_class))
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
