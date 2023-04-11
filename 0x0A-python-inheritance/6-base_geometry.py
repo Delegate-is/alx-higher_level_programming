6069ac70f52405eb27c6db889ab613428e42bd44
Delegat-is <kelvinjuma903@gmail.com>
2023-04-11 06:47:21 -0700

    Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+An empty class BaseGeometry representation
+"""
+
+
+class BaseGeometry:
+    """ empty class"""
+    pass
+
+    def area(self):
+        """Area function"""
+        raise Exception("area() is not implemented")
