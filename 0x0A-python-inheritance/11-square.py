7be3550377e5261881a7be16a7155e064e932af4
Delegat-is <kelvinjuma903@gmail.com>
2023-04-12 14:25:50 -0700

    Square__size

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,3 +16,5 @@ class Square(Rectangle):
         self.__size = size
     def __str__(self):
         return (f"[Square] {self.__size}/{self.__size}")
+    def area(self):
+        return self.__size ** 2
