6ce09cd841f68bd6e175057fcc9e7e46ad415a5c
Delegat-is <kelvinjuma903@gmail.com>
2023-04-12 14:01:08 -0700

    __-dict__ dictionary representation

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,13 +4,15 @@ Class Student
 """
 
 
-def __init__(self, first_name, last_name, age):
-    """function defines Student class attributes"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+clas Student:
+    """initialization of student class"""
+    def __init__(self, first_name, last_name, age):
+        """function defines Student class attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def to_json(self):
-    """function retrieving dictionary representation of a
-    Student instance"""
-    return self.__dict__
+    def to_json(self):
+        """function retrieving dictionary representation of a
+        Student instance"""
+        return self.__dict__
