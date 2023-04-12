71566a269a103eaffcd122d583cf84a16df64c0d
Delegat-is <kelvinjuma903@gmail.com>
2023-04-12 12:57:18 -0700

    JSON.dumps meth

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Function returning JSON object representation
+"""
+
+
+def to_json_string(my_obj):
+    """Return JSON representation of object"""
+    return (json.dumps(my_obj)
