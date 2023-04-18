453bf1ac6042d27e9088e4585b816191a02cb2c6
Delegat-is <kelvinjuma903@gmail.com>
2023-04-18 00:40:50 -0700

    Fourth indentation after (

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,6 +3,7 @@
 Module containing Rectangle
 Inheriting from the Rectangle class
 """
+from models.base import Base
 from models.rectangle import Rectangle
 
 
@@ -16,6 +17,9 @@ class Square(Rectangle):
             @x: coordinate x
             @y: coordinate y
             @id: Square identity
+        Raises:
+            TypeError if input is non integer.
+            ValueError if either width or height equals or is under zero.
         """
         super().__init__(size, size, x, y, id)
 
@@ -27,12 +31,17 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """Getting or setting Square's size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
     def __str__(self):
         """Returning str() and print() Square representation"""
-        return "[Square] ({}) {}/{}".format(self.id, self.x, self.y, self.size)
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.size)
 
     def to_dictionary(self):
         """Returning Square dictionary representation"""
