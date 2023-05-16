9e57eb9eb5368bf5f237ece0445fb0b964350210
Delegat-is <kelvinjuma903@gmail.com>
2023-05-16 13:00:59 -0700

    0x0D. SQL - Introduction

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Removing all records of second_table score <= 5 from database hbtn_0c_0
+DELETE FROM `second_table`
+WHERE `score` <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- Computing score average of all records in second_table
+SELECT AVG(`score`) AS `average` FROM `second_table`;
