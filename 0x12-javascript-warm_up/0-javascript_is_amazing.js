1fa984bb16dcd414a12392c0f91d69956410605d
Delegat-is <kelvinjuma903@gmail.com>
2023-06-12 13:09:42 -0700

    JavaScript is Amazing

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// My first JS project
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,5 @@
+JavaScript
+The introduction
+Project 0x12. 
+Its the JavaScript - Warm up
+JavaScript
