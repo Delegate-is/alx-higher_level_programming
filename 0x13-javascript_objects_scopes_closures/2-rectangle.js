499bac59a37c9cdd6ae5f80eeef3261db3ef104b
Delegat-is <kelvinjuma903@gmail.com>
2023-06-13 13:06:49 -0700

    print method

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 class Rectangle {
-	if (w => 0 && h => 0){
-		constructor (w, h) {
-			this.width = w;
-			this.height = h;
-		}
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 
 module.exports = Rectangle;
