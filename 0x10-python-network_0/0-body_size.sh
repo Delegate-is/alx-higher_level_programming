4704b433b4d3a962962df781c87264541cd8b26b
Delegat-is <kelvinjuma903@gmail.com>
2023-06-30 14:21:12 -0700

    Curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+# Bash script that takes in a URL, sends a request to that URL
+# and displays the size of the body of the response
+# The size must be displayed in bytes
 curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
-
