d50877e8a0e43f7041af62027c3878a256fcd937
Delegat-is <kelvinjuma903@gmail.com>
2023-07-04 12:13:49 -0700

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,4 +3,11 @@
 
 import urllib
 from urllib import request
-resp = request.urlopen("https://alx-intranet.hbtn.io/status")
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as page:
+        content = page.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
